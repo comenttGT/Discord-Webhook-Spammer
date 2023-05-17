aceb32486a50c44711e762a96726f698bdafaec7
comenttGT <100043986+comenttGT@users.noreply.github.com>
2023-05-17 19:43:45 +0200

    DWS-Tool v2 Source

---
+++ DiscordWebHookSpammer/DiscordWebHookSpammer/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace DiscordWebHookSpammer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.webhook = new System.Windows.Forms.TextBox();
+            this.customMessage = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.veryfastspam = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.time = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.customBotName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ink Free", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(460, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Discord WebHook Spammer";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Ink Free", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(87, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "WebHook:";
+            // 
+            // webhook
+            // 
+            this.webhook.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.webhook.Location = new System.Drawing.Point(168, 143);
+            this.webhook.Name = "webhook";
+            this.webhook.Size = new System.Drawing.Size(332, 27);
+            this.webhook.TabIndex = 2;
+            this.webhook.TextChanged += new System.EventHandler(this.webhook_TextChanged);
+            // 
+            // customMessage
+            // 
+            this.customMessage.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customMessage.Location = new System.Drawing.Point(168, 183);
+            this.customMessage.Name = "customMessage";
+            this.customMessage.Size = new System.Drawing.Size(332, 27);
+            this.customMessage.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Ink Free", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(87, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Message:";
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(199, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(267, 30);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Send Message";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // veryfastspam
+            // 
+            this.veryfastspam.AutoSize = true;
+            this.veryfastspam.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.veryfastspam.Location = new System.Drawing.Point(289, 418);
+            this.veryfastspam.Name = "veryfastspam";
+            this.veryfastspam.Size = new System.Drawing.Size(69, 24);
+            this.veryfastspam.TabIndex = 6;
+            this.veryfastspam.Text = "Spam";
+            this.veryfastspam.UseVisualStyleBackColor = true;
+            this.veryfastspam.CheckedChanged += new System.EventHandler(this.veryfastspam_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-16, 402);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(655, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "_________________________________________________________________________________" +
+    "___________________________";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(285, 379);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 23);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Other:";
+            // 
+            // time
+            // 
+            this.time.Tick += new System.EventHandler(this.time_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DiscordWebHookSpammer.Properties.Resources.youtube_logo_res;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 418);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(52, 45);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::DiscordWebHookSpammer.Properties.Resources.discordio_logo_res;
+            this.pictureBox2.Location = new System.Drawing.Point(70, 418);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(52, 45);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // customBotName
+            // 
+            this.customBotName.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customBotName.Location = new System.Drawing.Point(168, 219);
+            this.customBotName.Name = "customBotName";
+            this.customBotName.Size = new System.Drawing.Size(332, 27);
+            this.customBotName.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Ink Free", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(87, 223);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 19);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Bot Name";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Unispace", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(32, 223);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 19);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "NEW!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(628, 469);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.customBotName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.veryfastspam);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.customMessage);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.webhook);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Made by ComenttGT";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox webhook;
+        private System.Windows.Forms.TextBox customMessage;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox veryfastspam;
+        private System.Windows.Forms.Timer time;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox customBotName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ DiscordWebHookSpammer/DiscordWebHookSpammer/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using DiscordWebHookSpammer.res;
+
+namespace DiscordWebHookSpammer
+{
+    public partial class Form1 : Form
+    {
+        private int spamornot;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sendmsg(webhook.Text, customMessage.Text, customBotName.Text);
+        }
+
+        private static void sendmsg(string webhook, string message, string botname)
+        {
+            connection.post(webhook, new System.Collections.Specialized.NameValueCollection()
+            {
+                {
+                    "username",
+                    botname
+                },
+                {
+                    "content",
+                    message
+                }
+            });
+
+        }
+
+        
+
+        
+
+        private void veryfastspam_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            time.Start();
+        }
+
+        private void webhook_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void time_Tick(object sender, EventArgs e)
+        {
+            if (veryfastspam.Checked)
+            {
+                sendmsg(webhook.Text, customMessage.Text, customBotName.Text);
+            }
+            else
+            {
+
+            };
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.youtube.com/@comenttGT");
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://discord.gg/sUg38TnqQn");
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
