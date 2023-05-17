1dac385804bca17f6f12d874ac254945e104df97
comenttGT <100043986+comenttGT@users.noreply.github.com>
2023-05-17 19:29:57 +0200

    DWS-Tool-V1-Source

---
+++ DiscordWebHookSpammer/DiscordWebHookSpammer/res/connection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+
+namespace DiscordWebHookSpammer.res
+{
+    internal class connection
+    {
+        public static byte[] post(string url, NameValueCollection pair)
+        {
+            using (WebClient client = new WebClient())
+            {
+                return client.UploadValues(url, pair);
+            }
+        }
+    }
+}
